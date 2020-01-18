c65eca76cb92e034917265b3ab784de391cd1a73
omeganix <N.T.KRIEGER@GMAIL.COM>
2020-01-17 19:21:46 -0500

    same as the last except this compiles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TimeAgo from 'timeago-react';
@@ -214,7 +212,7 @@ class PostFooter extends React.Component{
     this.moreClick = this.moreClick.bind(this);
   }
   commentClick(){
-    Example();
+    {/*THIS FEATURE NOT SUPPORTED MODAL BOX*/}
   }
   moreClick(){
     this.setState(state => ({
@@ -334,36 +332,5 @@ class CollapsableMenuItemSimple extends React.Component{
   </li>
   }
 }
-
-function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <div>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
-}
-
 
 ReactDOM.render(<Reddit />, document.getElementById("app")); 
